90ef51901a3896e40e5c2bff232e88017e505718
hrustad1017 <35585553+hrustad1017@users.noreply.github.com>
2019-10-20 06:40:25 -0700

    fixed body-parser deprecation warning

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ let sessionOptions = {
 }
 app.use(session(sessionOptions));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
